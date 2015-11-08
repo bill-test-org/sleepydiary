1888c46af6390eb692d8f8e1f8ea4407491b34f6
skeetm0n <william.ryann@gmail.com>
2015-11-08 02:39:15 +0000

    Added a barebones devise gem login screen

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  
+  ### Begin Sleepy-Diary stuff ###
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  ### End Sleepy-Diary stuff ###
 end
