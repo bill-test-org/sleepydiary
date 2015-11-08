a57e67ff8e7184ea9f72e29e105034a8b0be3de2
skeetm0n <william.ryann@gmail.com>
2015-11-08 21:27:32 +0000

    Putting a file into an empty directory so that Heroku can deploy on production

---
+++ app/assets/templates/deletethis.js
@@ -0,0 +1,4 @@
+// TODO delete this file once the /templates directory contains files
+// Heroku barfs on the fact that application.js requires a /templates dir but
+// the /templates dir does not contain any files.  Also the Github repo
+// does not add folders with no files in them.
